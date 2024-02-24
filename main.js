428a8c51df105881747137ec3b5e90cfccc2c2e2
Evgene Kuro <lilkurokokoro@gmail.com>
2024-02-25 02:01:15 +0300

    add the cube

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+camera.position.z = 5;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
