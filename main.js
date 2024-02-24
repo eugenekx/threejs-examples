3e9daded6a4bd2e28625698291404e26181bf436
Evgene Kuro <lilkurokokoro@gmail.com>
2024-02-25 02:21:10 +0300

    add sphere and cone

--- main.js
+++ main.js
@@ -10,12 +10,22 @@ const camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 5;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
 const texture = new THREE.TextureLoader().load("textures/cube-texture.jfif");
 const material = new THREE.MeshBasicMaterial({ map: texture });
-const cube = new THREE.Mesh(geometry, material);
 
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cube = new THREE.Mesh(cubeGeometry, material);
 scene.add(cube);
+
+const coneGeometry = new THREE.ConeGeometry(1, 1, 4);
+const cone = new THREE.Mesh(coneGeometry, material);
+cone.position.x = 2;
+scene.add(cone);
+
+const sphereGeometry = new THREE.SphereGeometry(0.5);
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = -2;
+scene.add(sphere);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -25,5 +35,9 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   cube.rotateY(0.01);
+
+  cone.rotateY(0.01);
+
+  sphere.rotateY(0.01);
 }
 animate();
