7468bc7ca489c673098c8ecf87fb71eb2533a56e
Evgene Kuro <lilkurokokoro@gmail.com>
2024-02-25 03:19:40 +0300

    add new textures

--- main.js
+++ main.js
@@ -10,22 +10,55 @@ const camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 5;
 
-const texture = new THREE.TextureLoader().load("textures/cube-texture.jfif");
-const material = new THREE.MeshPhongMaterial({ map: texture });
+const albedoWoodTexture = new THREE.TextureLoader().load(
+  "textures/wooden-planks/albedo.png"
+);
+const normalWoodTexture = new THREE.TextureLoader().load(
+  "textures/wooden-planks/normal.png"
+);
+
+const materialWood = new THREE.MeshPhongMaterial({
+  map: albedoWoodTexture,
+  normalMap: normalWoodTexture,
+});
+
+const albedoBrickTexture = new THREE.TextureLoader().load(
+  "textures/brick-wall/albedo.png"
+);
+const normalBrickTexture = new THREE.TextureLoader().load(
+  "textures/brick-wall/normal.png"
+);
+
+const materialBrick = new THREE.MeshPhongMaterial({
+  map: albedoBrickTexture,
+  normalMap: normalBrickTexture,
+});
+
+const albedoStoneTexture = new THREE.TextureLoader().load(
+  "textures/mossy-stone/albedo.png"
+);
+const normalStoneTexture = new THREE.TextureLoader().load(
+  "textures/mossy-stone/normal.png"
+);
+
+const materialStone = new THREE.MeshPhongMaterial({
+  map: albedoStoneTexture,
+  normalMap: normalStoneTexture,
+});
+
+const sphereGeometry = new THREE.SphereGeometry(0.5);
+const sphere = new THREE.Mesh(sphereGeometry, materialStone);
+sphere.position.x = -2;
+scene.add(sphere);
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cube = new THREE.Mesh(cubeGeometry, material);
+const cube = new THREE.Mesh(cubeGeometry, materialBrick);
 scene.add(cube);
 
 const coneGeometry = new THREE.ConeGeometry(1, 1, 4);
-const cone = new THREE.Mesh(coneGeometry, material);
+const cone = new THREE.Mesh(coneGeometry, materialWood);
 cone.position.x = 2;
 scene.add(cone);
-
-const sphereGeometry = new THREE.SphereGeometry(0.5);
-const sphere = new THREE.Mesh(sphereGeometry, material);
-sphere.position.x = -2;
-scene.add(sphere);
 
 const light = new THREE.PointLight(0xffffff, 2);
 light.position.set(0, 0, 2);
@@ -42,13 +75,13 @@ function animate(time) {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 
+  sphere.rotateY(0.01);
+  sphere.position.y = lfo(-0.5);
+
   cube.rotateY(0.01);
   cube.position.y = lfo();
 
   cone.rotateY(0.01);
   cone.position.y = lfo(0.5);
-
-  sphere.rotateY(0.01);
-  sphere.position.y = lfo(-0.5);
 }
 animate();
