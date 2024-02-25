7b28003abb1e0a8cfd12e03fc0fbf0e15e47b73a
Evgene Kuro <lilkurokokoro@gmail.com>
2024-02-25 03:30:05 +0300

    add mouse event (picking)

--- main.js
+++ main.js
@@ -1,5 +1,84 @@
 import * as THREE from "three";
 
+class PickHelper {
+  constructor() {
+    this.raycaster = new THREE.Raycaster();
+    this.pickedObject = null;
+    this.pickedObjectSavedColor = 0;
+  }
+  pick(normalizedPosition, scene, camera, time) {
+    // restore the color if there is a picked object
+    if (this.pickedObject) {
+      this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
+      this.pickedObject = undefined;
+    }
+
+    // cast a ray through the frustum
+    this.raycaster.setFromCamera(normalizedPosition, camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+    if (intersectedObjects.length) {
+      // pick the first object. It's the closest one
+      this.pickedObject = intersectedObjects[0].object;
+      // save its color
+      this.pickedObjectSavedColor =
+        this.pickedObject.material.emissive.getHex();
+      // set its emissive color to flashing red/yellow
+      this.pickedObject.material.emissive.setHex(
+        (time * 8) % 2 > 1 ? 0xffff00 : 0xff0000
+      );
+    }
+  }
+}
+
+const pickPosition = { x: 0, y: 0 };
+clearPickPosition();
+
+function getCanvasRelativePosition(event) {
+  const canvas = document.querySelector("canvas");
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: ((event.clientX - rect.left) * canvas.width) / rect.width,
+    y: ((event.clientY - rect.top) * canvas.height) / rect.height,
+  };
+}
+
+function setPickPosition(event) {
+  const canvas = document.querySelector("canvas");
+  const pos = getCanvasRelativePosition(event);
+  pickPosition.x = (pos.x / canvas.width) * 2 - 1;
+  pickPosition.y = (pos.y / canvas.height) * -2 + 1; // note we flip Y
+}
+
+function clearPickPosition() {
+  // unlike the mouse which always has a position
+  // if the user stops touching the screen we want
+  // to stop picking. For now we just pick a value
+  // unlikely to pick something
+  pickPosition.x = -100000;
+  pickPosition.y = -100000;
+}
+
+window.addEventListener("mousemove", setPickPosition);
+window.addEventListener("mouseout", clearPickPosition);
+window.addEventListener("mouseleave", clearPickPosition);
+
+window.addEventListener(
+  "touchstart",
+  (event) => {
+    // prevent the window from scrolling
+    event.preventDefault();
+    setPickPosition(event.touches[0]);
+  },
+  { passive: false }
+);
+
+window.addEventListener("touchmove", (event) => {
+  setPickPosition(event.touches[0]);
+});
+
+window.addEventListener("touchend", clearPickPosition);
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -68,6 +147,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const pickHelper = new PickHelper();
+
 function animate(time) {
   const period = 500; // ms
   const lfo = (delay = 0) => Math.sin(time / period + delay);
@@ -83,5 +164,9 @@ function animate(time) {
 
   cone.rotateY(0.01);
   cone.position.y = lfo(0.5);
+
+  time *= 0.001;
+
+  pickHelper.pick(pickPosition, scene, camera, time);
 }
 animate();
